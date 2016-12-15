9f4cc9f121757a5897b4a675fd4adc096bffa918
Josh Karlin <jkarlin@users.noreply.github.com>
2016-12-15 09:37:46 -0500

    size

--- www/jkarlin/timing/size.php
+++ www/jkarlin/timing/size.php
@@ -3,7 +3,9 @@ header("Cache-Control: no-store");
 header("Content-Type: text/html");
 $size = $_GET['size'];
 
-if ($size <= 1024*1024*50) {
+if ($size > 1024*1024*50) {
+  echo "Request too large! Please keep it <= 50MB";
+} else {
   echo str_pad("a", $size, 'a');
 }
 ?>
