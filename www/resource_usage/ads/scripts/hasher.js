63e8c9ec077f523f5cfeb3bfcfb37f0f40fb2c6b
johnivdel <40465606+johnivdel@users.noreply.github.com>
2019-03-14 15:11:45 -0400

    Update hasher.js

--- www/resource_usage/ads/scripts/hasher.js
+++ www/resource_usage/ads/scripts/hasher.js
@@ -1,8 +1,8 @@
-var NUM_PARALLEL_HASHERS = 8;
+var NUM_PARALLEL_HASHERS = 40;
 var seed = "starting seed for hash";
 function doHash() {
-        seed = CryptoJS.MD5(seed);
-        console.log(seed);
+    seed = CryptoJS.MD5(seed);
+    console.log(seed);
     setTimeout(doHash, 0);
 }
 
