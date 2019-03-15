b677245c5e8fc7ac3e6d63a4a3747be0198e945f
johnivdel <40465606+johnivdel@users.noreply.github.com>
2019-03-15 14:50:27 -0400

    Update dfp.js

--- www/resource_usage/ads/scripts/dfp.js
+++ www/resource_usage/ads/scripts/dfp.js
@@ -3,6 +3,7 @@ frame.width = 400;
 frame.height = 400;
 var urlParams = new URLSearchParams(window.location.search);
 var site = urlParams.get('site')
+console.log(site);
 if (site)
   frame.src = site;
 else
