e3bdee48ac5ef8e94f166cfe14703bcc83e13e39
xyaoinum <xyaof3@126.com>
2019-03-25 17:04:38 -0400

    Update sw.js

--- www/yao/service_worker/sw.js
+++ www/yao/service_worker/sw.js
@@ -1,1 +1,6 @@
-console.log('Hello world');
+self.addEventListener("fetch", event => {
+  let url = new URL(event.request.url);
+  if (url.pathname.startsWith("/test")) {
+    event.respondWith(new Response("Hello from worker!"));
+  }
+});
