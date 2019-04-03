e73358756dba001976fd0a5fbaad4f5e19d69da7
xyaoinum <xyaof3@126.com>
2019-04-03 17:24:04 -0400

    Update sw.js

--- www/yao/service_worker/sw.js
+++ www/yao/service_worker/sw.js
@@ -1,5 +1,1 @@
-self.addEventListener("fetch", event => {
-  if (event.request.url.indexOf("/yao/service_worker/test1.html") != -1) {
-    event.respondWith(fetch(event.request));
-  }
-});
+importScripts('empty.js');
