aac45ea99446ca8d2fb778aed69d3c7e3769f4be
xyaoinum <xyaof3@126.com>
2019-04-03 13:56:17 -0400

    Update sw.js

--- www/yao/service_worker/sw.js
+++ www/yao/service_worker/sw.js
@@ -1,5 +1,11 @@
-self.addEventListener("fetch", event => {
-    if (event.request.url.indexOf("/yao/service_worker/test1.html") != -1) {
-      event.respondWith(fetch(event.request));
-    }
+// self.addEventListener("fetch", event => {
+//    if (event.request.url.indexOf("/yao/service_worker/test1.html") != -1) {
+//      event.respondWith(fetch(event.request));
+//    }
+// });
+
+fetch("/yao/service_worker/test1.html").then(response => {
+    console.log("Response:", response);
+}).catch(err => {
+    console.error("Error while fetching:", err);
 });
