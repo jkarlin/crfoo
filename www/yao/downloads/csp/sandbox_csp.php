dd82cae9310600243722f933e9dd80b16d0f1a2b
xyaoinum <xyaof3@126.com>
2019-04-08 15:55:28 -0400

    Update sandbox_csp.php

--- www/yao/downloads/csp/sandbox_csp.php
+++ www/yao/downloads/csp/sandbox_csp.php
@@ -3,5 +3,5 @@ $path = 'test.html';
 header('Content-Type: text/html');
 header('Content-Transfer-Encoding: binary');
 header('Accept-Ranges: bytes');
-header('Content-Security-Policy: sandbox');
+header('Content-Security-Policy: sandbox allow-scripts');
 readfile($path);
