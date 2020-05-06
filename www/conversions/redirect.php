d29dade98fff960e12b336a62e92d097e4fd8ee2
John Delaney <40465606+johnivdel@users.noreply.github.com>
2020-05-06 13:48:17 -0400

    Update redirect.php

--- www/conversions/redirect.php
+++ www/conversions/redirect.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://cr2.kungfoo.net/.well-known/register-conversion?conversion-data=2");
+header("Location: https://cr.kungfoo.net/.well-known/register-conversion?conversion-data=2");
 exit;
 ?>
