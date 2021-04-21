a48dec10709a7bb7120467e625c2acc1e309a6d2
Yao Xiao <xyaof3@126.com>
2021-04-21 02:42:11 -0400

    Update test2.js

--- www/yao/shared_storage/test2.js
+++ www/yao/shared_storage/test2.js
@@ -1,3 +1,10 @@
-qwe
-asd
-zxc
+class TestTestTest {
+  async run(urls) {
+    var key = "testtesttest";
+    var values = sharedStorage.read(key);
+    console.log(values);
+    return 2;    
+  }
+}
+
+registerURLSelectionOperation("test-operation", TestTestTest);
