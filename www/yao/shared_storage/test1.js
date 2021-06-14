5f63702c3ec2a7916165e690179753ea0fd926f2
Yao Xiao <xyaof3@126.com>
2021-06-14 15:30:07 -0400

    Update test1.js

--- www/yao/shared_storage/test1.js
+++ www/yao/shared_storage/test1.js
@@ -1,6 +1,9 @@
 class TestTestTest {
   async run(urls, options) {
-    return 1;
+//     return 1;
+    return new Promise(function(resolve) {
+        setTimeout(() => resolve(1), 500);
+    });
 //     if (options.name == "experimentA") {
 //       return 1;
 //     } else {
