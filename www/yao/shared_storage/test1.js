a99a1456d9324a9e7ccdaca66df46f3e13e3879b
Yao Xiao <xyaof3@126.com>
2021-06-16 21:43:26 -0400

    Update test1.js

--- www/yao/shared_storage/test1.js
+++ www/yao/shared_storage/test1.js
@@ -1,10 +1,10 @@
 class TestTestTest {
-  async run(urls, options) {
+  async run(urls, data) {
 //     return 2;
     
 //     return sharedStorage.set("key333", "value333");
     
-    if (options.name == "experimentA") {
+    if (data.name == "experimentA") {
       return 1;
     } else {
       return 2; 
