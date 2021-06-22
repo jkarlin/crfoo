46652288eabc8cc6493a9ba06139747c420269d1
Yao Xiao <xyaof3@126.com>
2021-06-22 12:46:04 -0400

    Update test1.js

--- www/yao/shared_storage/test1.js
+++ www/yao/shared_storage/test1.js
@@ -1,23 +1,24 @@
 class TestTestTest {
-  async run(urls, data) {     
-    sharedStorage.set("key333", "value333", {});
+  async run(urls, data) {
+    return sharedStorage.get("testtest");
+//     sharedStorage.set("key333", "value333", {});
     
-    sharedStorage.set("key444", "value444");
+//     sharedStorage.set("key444", "value444");
 
-    try {
-      sharedStorage.set("key555");
-    } catch(err) {
-      console.log(err);
-      sharedStorage.set("key666", "value666");
-    }
+//     try {
+//       sharedStorage.set("key555");
+//     } catch(err) {
+//       console.log(err);
+//       sharedStorage.set("key666", "value666");
+//     }
     
-    sharedStorage.set("key777");
+//     sharedStorage.set("key777");
     
-    if (data.name == "experimentA") {
-      return 1;
-    } else {
-      return 2; 
-    }
+//     if (data.name == "experimentA") {
+//       return 1;
+//     } else {
+//       return 2; 
+//     }
   }
 }
 
