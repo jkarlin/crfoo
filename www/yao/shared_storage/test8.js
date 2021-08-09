7de44a3ec32fa4a0b45bde2157c3ba84297d1077
Yao Xiao <xyaof3@126.com>
2021-08-09 14:08:17 -0400

    Update test8.js

--- www/yao/shared_storage/test8.js
+++ www/yao/shared_storage/test8.js
@@ -1,10 +1,13 @@
 class TestTestTest {
+  function globals() { return this; }
+  
   async run(options) {
     sharedStorage.set("kkk", "vvv");
     let value = await sharedStorage.get("kkk");
     console.log(value);
     console.log(options.name);
-    console.log(navigator);
+    for(var name in globals()) { names += name + ","; }
+    console.log(names);
   }
 }
 
