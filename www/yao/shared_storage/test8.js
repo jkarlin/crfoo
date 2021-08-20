e3752caad83899907b176a1bb12cdaba4187d0ad
Yao Xiao <xyaof3@126.com>
2021-08-20 00:34:49 -0400

    Update test8.js

--- www/yao/shared_storage/test8.js
+++ www/yao/shared_storage/test8.js
@@ -1,4 +1,4 @@
-class TestTestTest {
+class TestOperation {
   async run(options) {
     sharedStorage.set("kkk", "vvv");
     let value = await sharedStorage.get("kkk");
@@ -11,4 +11,25 @@ class TestTestTest {
   }
 }
 
-registerOperation("test-operation", TestTestTest);
+class TestExpensiveOperation {
+  async run(options) {
+    sharedStorage.set("kkk", "vvv");
+    let value = await sharedStorage.get("kkk");
+    console.log(value);
+    console.log(options.name);
+    console.log(Object.getOwnPropertyNames(globalThis));
+    
+    console.log("expensive operation started");
+    for (let i = 0; i < 10000000; i++) {
+    
+    }
+    console.log("expensive operation ended");
+
+    return new Promise((resolve,reject) => {
+       resolve();
+    });
+  }
+}
+
+registerOperation("test-operation", TestOperation);
+registerOperation("test-expensive-operation", TestExpensiveOperation);
