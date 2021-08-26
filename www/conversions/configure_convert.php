20fa60f2094e7061a1c06f6855fde072403d6696
Andrew Paseltiner <apaseltiner@google.com>
2021-08-26 12:28:12 -0400

    Add dedup-key support for triggers

--- www/conversions/configure_convert.php
+++ www/conversions/configure_convert.php
@@ -3,6 +3,7 @@ $query = http_build_query(array(
   'trigger-data' => $_GET['trigger-data'],
   'event-source-trigger-data' => $_GET['event-source-trigger-data'],
   'priority' => $_GET['priority'],
+  'dedup-key' => $_GET['dedup-key'],
 ));
 
 header('Location: https://cr.kungfoo.net/.well-known/attribution-reporting/trigger-attribution?' . $query);
