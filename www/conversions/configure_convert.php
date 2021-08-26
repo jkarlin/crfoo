fa49ecf9fb3cb786bd9fe49c0bc75345a4b9406e
Andrew Paseltiner <apaseltiner@google.com>
2021-08-26 11:12:31 -0400

    Add configurable attribution test

---
+++ www/conversions/configure_convert.php
@@ -0,0 +1,10 @@
+<?php
+$query = http_build_query(array(
+  'trigger-data' => $_GET['trigger-data'],
+  'event-source-trigger-data' => $_GET['event-source-trigger-data'],
+  'priority' => $_GET['priority'],
+));
+
+header('Location: https://cr.kungfoo.net/.well-known/attribution-reporting/trigger-attribution?' . $query);
+exit;
+?>
