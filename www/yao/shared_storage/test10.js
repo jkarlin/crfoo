ab478049b81da061dab116e3f86cb378070a5811
Yao Xiao <xyaof3@126.com>
2021-09-27 19:16:27 -0400

    Update test10.js

--- www/yao/shared_storage/test10.js
+++ www/yao/shared_storage/test10.js
@@ -1,7 +1,13 @@
 class TestOperation {
   async run(urls, data) {
-    console.log("===== run =======");
+    console.log("====== run started =====");
     console.log(urls);
+    for (let i = 0; i < 10000000; i++) {
+      if (i % 1000000 == 0)
+      console.log(i);
+    }
+    console.log("====== run finished =====");
+
     return 1;
   }
 }
