8a993c8e8eab1140a116138d3df02e789c584d80
Yao Xiao <xyaof3@126.com>
2021-10-27 18:05:10 -0400

    Update test10.js

--- www/yao/shared_storage/test10.js
+++ www/yao/shared_storage/test10.js
@@ -24,5 +24,24 @@ class TestOperationFail {
   }
 }
 
+class TestOperationFast {
+  async run(urls, data) {
+    console.log("====== run started =====");
+    console.log(urls);
+    console.log("====== run finished =====");
+    return 1;
+  }
+}
+
+class TestOperationFailFast {
+  async run(urls, data) {
+    console.log("====== run started =====");
+    console.log(urls);
+    console.log("====== run finished =====");
+  }
+}
+
 registerURLSelectionOperation("test-operation", TestOperation);
 registerURLSelectionOperation("test-operation-fail", TestOperationFail);
+registerURLSelectionOperation("test-operation-fast", TestOperationFast);
+registerURLSelectionOperation("test-operation-fail-fast", TestOperationFailFast);
