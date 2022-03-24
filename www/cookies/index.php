5e0aa588d7a358f59d49ac262b5eee8dc2f1ea01
DCtheTall <dylancutler@google.com>
2022-03-24 19:22:18 -0400

    Add CHIPS origin trial token

--- www/cookies/index.php
+++ www/cookies/index.php
@@ -1,3 +1,7 @@
+<?php
+header('Origin-Trial: AxaaagJW8HOJtvFrCmbiobCcA64HfLOesA92jfHiSWe81+/MkKwpEBGKfU/Y/u8rG8Owzr2n6SlqEszDc6CRNggAAABveyJvcmlnaW4iOiJodHRwczovL2NyMi5rdW5nZm9vLm5ldDo0NDMiLCJmZWF0dXJlIjoiUGFydGl0aW9uZWRDb29raWVzIiwiZXhwaXJ5IjoxNjU1MjUxMTk5LCJpc1RoaXJkUGFydHkiOnRydWV9');
+header('Accet-CH: Sec-CH-Partitioned-Cookies');
+?>
 <!DOCTYPE html>
 <meta charset="utf-8">
 <button id="set-cookie">Set cookie (SameSite=None)</button>
--- www/cookies/partitioned-3p.php
+++ www/cookies/partitioned-3p.php
@@ -1,3 +1,5 @@
 <?php
 header('Set-Cookie: __Host-3P_partitioned=foobar; Secure; Path=/; SameSite=None; Partitioned; Max-Age=86400');
+header('Origin-Trial: AxaaagJW8HOJtvFrCmbiobCcA64HfLOesA92jfHiSWe81+/MkKwpEBGKfU/Y/u8rG8Owzr2n6SlqEszDc6CRNggAAABveyJvcmlnaW4iOiJodHRwczovL2NyMi5rdW5nZm9vLm5ldDo0NDMiLCJmZWF0dXJlIjoiUGFydGl0aW9uZWRDb29raWVzIiwiZXhwaXJ5IjoxNjU1MjUxMTk5LCJpc1RoaXJkUGFydHkiOnRydWV9');
+header('Accet-CH: Sec-CH-Partitioned-Cookies');
 ?>
