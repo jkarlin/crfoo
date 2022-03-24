6b32342d6ec681f141ca5e2f1e9279b893ef3c66
DCtheTall <dylancutler@google.com>
2022-03-24 19:30:57 -0400

    Fix Accept-CH typo and opt-in HTML to CHIPS OT

--- www/cookies/thirdparty.php
+++ www/cookies/thirdparty.php
@@ -1,9 +1,10 @@
 <?php
 header('Origin-Trial: AxaaagJW8HOJtvFrCmbiobCcA64HfLOesA92jfHiSWe81+/MkKwpEBGKfU/Y/u8rG8Owzr2n6SlqEszDc6CRNggAAABveyJvcmlnaW4iOiJodHRwczovL2NyMi5rdW5nZm9vLm5ldDo0NDMiLCJmZWF0dXJlIjoiUGFydGl0aW9uZWRDb29raWVzIiwiZXhwaXJ5IjoxNjU1MjUxMTk5LCJpc1RoaXJkUGFydHkiOnRydWV9');
-header('Accet-CH: Sec-CH-Partitioned-Cookies');
+header('Accept-CH: Sec-CH-Partitioned-Cookies');
 ?>
 <!DOCTYPE html>
 <meta charset="utf-8">
+<meta http-equiv="origin-trial" content="AxaaagJW8HOJtvFrCmbiobCcA64HfLOesA92jfHiSWe81+/MkKwpEBGKfU/Y/u8rG8Owzr2n6SlqEszDc6CRNggAAABveyJvcmlnaW4iOiJodHRwczovL2NyMi5rdW5nZm9vLm5ldDo0NDMiLCJmZWF0dXJlIjoiUGFydGl0aW9uZWRDb29raWVzIiwiZXhwaXJ5IjoxNjU1MjUxMTk5LCJpc1RoaXJkUGFydHkiOnRydWV9">
 <button id="set-cookie">Set cookie (SameSite=None)</button>
 <button id="set-partitioned-cookie">Set partitioned cookie (SameSite=None; Partitioned)</button>
 <div>document.cookie is <span id="document-cookie"></span></div>
