12811b26b28802d7dbc9099a8a7b03e8b6160e58
DCtheTall <dylancutler@google.com>
2022-03-24 19:52:24 -0400

    Add 3P token to iframe using JS

--- www/cookies/thirdparty.php
+++ www/cookies/thirdparty.php
@@ -4,12 +4,17 @@ header('Accept-CH: Sec-CH-Partitioned-Cookies');
 ?>
 <!DOCTYPE html>
 <meta charset="utf-8">
-<meta http-equiv="origin-trial" content="AxaaagJW8HOJtvFrCmbiobCcA64HfLOesA92jfHiSWe81+/MkKwpEBGKfU/Y/u8rG8Owzr2n6SlqEszDc6CRNggAAABveyJvcmlnaW4iOiJodHRwczovL2NyMi5rdW5nZm9vLm5ldDo0NDMiLCJmZWF0dXJlIjoiUGFydGl0aW9uZWRDb29raWVzIiwiZXhwaXJ5IjoxNjU1MjUxMTk5LCJpc1RoaXJkUGFydHkiOnRydWV9">
 <button id="set-cookie">Set cookie (SameSite=None)</button>
 <button id="set-partitioned-cookie">Set partitioned cookie (SameSite=None; Partitioned)</button>
 <div>document.cookie is <span id="document-cookie"></span></div>
 <button id="clear-cookies">Clear cookies</button>
 <script>
+
+// Third-party participants must add the token via JS.
+const tokenElement = document.createElement('meta');
+tokenElement.httpEquiv = 'origin-trial';
+tokenElement.content = 'AxaaagJW8HOJtvFrCmbiobCcA64HfLOesA92jfHiSWe81+/MkKwpEBGKfU/Y/u8rG8Owzr2n6SlqEszDc6CRNggAAABveyJvcmlnaW4iOiJodHRwczovL2NyMi5rdW5nZm9vLm5ldDo0NDMiLCJmZWF0dXJlIjoiUGFydGl0aW9uZWRDb29raWVzIiwiZXhwaXJ5IjoxNjU1MjUxMTk5LCJpc1RoaXJkUGFydHkiOnRydWV9';
+document.head.appendChild(tokenElement);
 
 for (const [id, endpoint] of [
   ['set-cookie', 'samesitenone-3p'],
