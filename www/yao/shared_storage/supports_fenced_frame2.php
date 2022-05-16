46566cfc10d355bc6eecba1f10a12ff0580041c6
Yao Xiao <xyaof3@126.com>
2022-05-16 16:59:03 -0400

    Create supports_fenced_frame2.php

---
+++ www/yao/shared_storage/supports_fenced_frame2.php
@@ -0,0 +1,15 @@
+<?php
+header("Supports-Loading-Mode: fenced-frame");
+?>
+<body>
+
+Hi there 222, this document is served with a 'Supports-Loading-Mode: fenced-frame' header.
+
+<p>The server time is: 
+
+<?php
+echo time();
+?>
+</p>
+
+</body>
