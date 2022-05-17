8bc3658a89e99589899771044d0417b952523ae7
Yao Xiao <xyaof3@126.com>
2022-05-17 13:15:53 -0400

    Update supports_fenced_frame.php

--- www/yao/shared_storage/supports_fenced_frame.php
+++ www/yao/shared_storage/supports_fenced_frame.php
@@ -17,7 +17,7 @@ echo time();
 <button onclick="myFunction5()">Same-site navigation, and then popup</button><br>
 <button onclick="myFunction6()">Cross-site navigation, and then popup</button><br>
 <input type="text" id="popup-delay" value="50"><br>
-<fencedframe id="frame1" mode="opaque-ads" src="supports_fenced_frame.php"></fencedframe>
+<fencedframe id="frame1" mode="opaque-ads" style="width: 100%" src="supports_fenced_frame.php"></fencedframe>
 
 <iframe id="iframe1" src="iframe.html"><iframe>
   
