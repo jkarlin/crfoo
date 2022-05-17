ce173eccf9971661ce0010ef422ad5f51172b898
Yao Xiao <xyaof3@126.com>
2022-05-16 22:53:05 -0400

    Update supports_fenced_frame.php

--- www/yao/shared_storage/supports_fenced_frame.php
+++ www/yao/shared_storage/supports_fenced_frame.php
@@ -17,17 +17,18 @@ echo time();
 <button onclick="myFunction3()">Reload</button><br>
 <button onclick="myFunction4()">Navigate self and then popup</button><br>
 <br>
-<button onclick="myFunction5()">Navigate self and then popup later</button><br>
-<input type="text" id="popup-delay"><br>
+<button onclick="myFunction5()">Same-site navigation, and then popup</button><br>
+<button onclick="myFunction6()">Cross-site navigation, and then popup</button><br>
+<input type="text" id="popup-delay" value="50"><br>
 
   
 <script>
 function myFunction() {
-  window.open("https://baidu.com", "_unfencedTop");
+  window.open("https://google.com", "_unfencedTop");
 }
   
 function myFunction2() {
-  window.open("https://baidu.com");
+  window.open("https://google.com");
 }
   
 function myFunction3() {
@@ -36,22 +37,29 @@ function myFunction3() {
   
 function myFunction4() {
   window.location.href = 'supports_fenced_frame2.php';
-  window.open("https://baidu.com");
+  window.open("https://google.com.com");
 }
 
 function myFunction4() {
   window.location.href = 'supports_fenced_frame2.php';
-  window.open("https://baidu.com");
+  window.open("https://google.com");
 }
   
 function myFunction5() {
   window.location.href = 'supports_fenced_frame2.php';
   let delay = Number(document.getElementById("popup-delay").value);
   window.setTimeout(() => {
-    window.open("https://baidu.com");
+    window.open("https://google.com");
   }, delay);
 }
-  
+
+function myFunction6() {
+  window.location.href = 'https://baidu.com';
+  let delay = Number(document.getElementById("popup-delay").value);
+  window.setTimeout(() => {
+    window.open("https://google.com");
+  }, delay);
+}
   
 </script>
   
