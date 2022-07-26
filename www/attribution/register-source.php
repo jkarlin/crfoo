ddb13664c12a118d2a1b56ce14a3e65fdeb66a5c
John Delaney <40465606+johnivdel@users.noreply.github.com>
2022-07-26 13:10:47 -0400

    Update register-source.php

--- www/attribution/register-source.php
+++ www/attribution/register-source.php
@@ -1,5 +1,5 @@
 <?php
 header("Attribution-Reporting-Register-Source: {\"source_event_id\"1: \", \"destination\": \"https://cr2.kungfoo.net\"}");
-header("Location: conv.html");
+header("Location: https://cr2.kungfoo.net/conv.html");
 ?>
 Something
