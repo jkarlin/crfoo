2fb5ed946f9b4d333067afbba490ce06e327f251
Yao Xiao <xyaof3@126.com>
2022-07-27 18:19:20 -0400

    Update observe_browsing_topics.php

--- www/yao/browsing_topics/observe_browsing_topics.php
+++ www/yao/browsing_topics/observe_browsing_topics.php
@@ -2,13 +2,8 @@
 header("Observe-Browsing-Topics: 1");
 ?>
 <body>
-<p>The server time is: 
 
-<?php
-echo time();
-print_r($_SERVER);
-?>
-</p>
+<pre><?php var_dump($_SERVER); ?></pre>
 
 <button onclick="function1()">Navigate self</button><br>
 <button onclick="function2()">Invoke document.browsingTopics()</button><br>
