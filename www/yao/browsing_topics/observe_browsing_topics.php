9a21b0f58c8062aaabcf75c63e84a77a2f55115a
Yao Xiao <xyaof3@126.com>
2022-07-27 15:14:56 -0400

    Update observe_browsing_topics.php

--- www/yao/browsing_topics/observe_browsing_topics.php
+++ www/yao/browsing_topics/observe_browsing_topics.php
@@ -10,10 +10,15 @@ echo time();
 </p>
 
 <button onclick="function1()">Navigate self</button><br>
-  
+<button onclick="function2()">Invoke document.browsingTopics()</button><br>
+
 <script>
 function function1() {
   window.location.href = 'observe_browsing_topics.php';
+}
+  
+function function2() {
+  document.browsingTopics();
 }
 </script>
   
