39701d7be9b98a4eb787e1498c3db59834cd5b22
John Delaney <40465606+johnivdel@users.noreply.github.com>
2022-07-28 12:40:47 -0400

    Update register-subresource.php

--- www/attribution/register-subresource.php
+++ www/attribution/register-subresource.php
@@ -1,5 +1,6 @@
 <?php
 header("Attribution-Reporting-Register-Trigger: {}");
 header("Set-Cookie: ar_debug=1; SameSite=None; Secure; HttpOnly");
+header("Access-Control-Expose-headers: Attribution-Reporting-Register-Trigger");
 ?>
 Something
