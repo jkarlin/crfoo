74cc49d66d225f8d4534a10e32935a0e67a1fd43
Andrew Paseltiner <apaseltiner@google.com>
2022-07-28 15:50:17 -0400

    Update register-subresource.php

--- www/attribution/register-subresource.php
+++ www/attribution/register-subresource.php
@@ -3,5 +3,6 @@ header('attribution-reporting-register-trigger: {"aggregatable_trigger_data":[{"
 header("Set-Cookie: ar_debug=1; SameSite=None; Secure; HttpOnly");
 header("Cross-Origin-Resource-Policy: cross-origin");
 header("Content-Security-Policy: script-src 'none'; object-src 'none'");
+header('x-content-type-options: nosniff');
 //header("Access-Control-Expose-headers: Attribution-Reporting-Register-Trigger");
 ?>
