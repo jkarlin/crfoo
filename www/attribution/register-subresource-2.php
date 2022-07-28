e14034a792a8682f580c70f5e5e75d655506d1f6
John Delaney <40465606+johnivdel@users.noreply.github.com>
2022-07-28 12:34:05 -0400

    Update and rename register_subresource_2.php to register-subresource-2.php

---
+++ www/attribution/register-subresource-2.php
@@ -0,0 +1,5 @@
+<?php
+header("Attribution-Reporting-Register-Source: {\"source_event_id\": \"1\", \"destination\": \"https://cr2.kungfoo.net\"}");
+header("Access-Control-Allow-Origin: *);
+?>
+Something
