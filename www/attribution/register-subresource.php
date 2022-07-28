360f65fc88335f5d32b2f7f0f8e880ffdd835e95
John Delaney <40465606+johnivdel@users.noreply.github.com>
2022-07-28 13:43:39 -0400

    Update register-subresource.php

--- www/attribution/register-subresource.php
+++ www/attribution/register-subresource.php
@@ -2,6 +2,7 @@
 header("Attribution-Reporting-Register-Trigger: {}");
 header("Set-Cookie: ar_debug=1; SameSite=None; Secure; HttpOnly");
 header("Cross-Origin-Resource-Policy: cross-origin");
+header("Content-Security-Policy: script-src 'none'; object-src 'none'");
 //header("Access-Control-Expose-headers: Attribution-Reporting-Register-Trigger");
 ?>
 Something
