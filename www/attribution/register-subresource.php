b2255287003c2a027ea87eaf5218d6dce8fd6039
John Delaney <40465606+johnivdel@users.noreply.github.com>
2022-07-28 12:36:29 -0400

    Update register-subresource.php

--- www/attribution/register-subresource.php
+++ www/attribution/register-subresource.php
@@ -1,4 +1,4 @@
 <?php
-header("Attribution-Reporting-Register-Source: {\"source_event_id\": \"1\", \"destination\": \"https://cr2.kungfoo.net\"}");
+header("Attribution-Reporting-Register-Trigger: {}");
 ?>
 Something
