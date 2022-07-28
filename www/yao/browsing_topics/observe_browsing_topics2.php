ed58244e10ebfeba998ff45934b037156fca5e75
Yao Xiao <xyaof3@126.com>
2022-07-28 10:41:47 -0400

    Create observe_browsing_topics2.php

---
+++ www/yao/browsing_topics/observe_browsing_topics2.php
@@ -0,0 +1,21 @@
+<?php
+header("Observe-Browsing-Topics: 1");
+?>
+<body>
+
+<pre><?php var_dump($_SERVER); ?></pre>
+
+<button onclick="function1()">Navigate self</button><br>
+<button onclick="function2()">Invoke document.browsingTopics()</button><br>
+
+<script>
+function function1() {
+  window.location.href = 'observe_browsing_topics.php';
+}
+  
+function function2() {
+  document.browsingTopics();
+}
+</script>
+  
+</body>
