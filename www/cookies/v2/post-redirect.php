f31e42e55c8f40f56ec87d99a65ce5023a873730
Dylan Cutler <dylancutler@google.com>
2023-01-31 14:16:24 -0500

    POST redirect should handle CORS preflight using OPTIONS method

--- www/cookies/v2/post-redirect.php
+++ www/cookies/v2/post-redirect.php
@@ -1,6 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' ||
+    $_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
   header(
       'Location', $_POST['location'] . '?cookie=' . getallheaders()['Cookie']);
   http_response_code(200);
