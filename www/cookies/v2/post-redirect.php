a930ea35973dd278c56f80b75c9b6053c2d8a4af
Dylan Cutler <dylancutler@google.com>
2023-01-31 14:14:39 -0500

    Add HTTP OK status to POST redirect

--- www/cookies/v2/post-redirect.php
+++ www/cookies/v2/post-redirect.php
@@ -3,6 +3,7 @@ header('Access-Control-Allow-Origin: *');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   header(
       'Location', $_POST['location'] . '?cookie=' . getallheaders()['Cookie']);
+  http_response_code(200);
 } else {
   http_response_code(404);
 }
