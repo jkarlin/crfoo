c284533f8ae45fd6fd6af3a961e12f1f2e85c0ed
Dylan Cutler <dylancutler@google.com>
2023-01-31 14:29:22 -0500

    Use 301 response code for POST redirect

--- www/cookies/v2/post-redirect.php
+++ www/cookies/v2/post-redirect.php
@@ -1,10 +1,9 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-if ($_SERVER['REQUEST_METHOD'] === 'POST' ||
-    $_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   header(
       'Location: ' . $_POST['location'] . '?cookie=' . getallheaders()['Cookie']);
-  http_response_code(200);
+  http_response_code(301);
 } else {
   http_response_code(404);
 }
