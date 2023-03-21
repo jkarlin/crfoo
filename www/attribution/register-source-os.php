d54468400ef1878acea25a72f3f41133ed163e41
John Delaney <40465606+johnivdel@users.noreply.github.com>
2023-03-21 17:48:37 -0400

    Update register-source-os.php

--- www/attribution/register-source-os.php
+++ www/attribution/register-source-os.php
@@ -1,5 +1,5 @@
 <?php
-header("Attribution-Reporting-Register-OS-Source: https://cr.kungfoo.net/register-source.php");
+header("Attribution-Reporting-Register-OS-Source: \"https://cr.kungfoo.net/register-source.php\"");
 header("Location: https://cr2.kungfoo.net/attribution/conv.html");
 ?>
 Something
