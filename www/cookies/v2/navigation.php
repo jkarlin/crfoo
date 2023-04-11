7752dcb972ac3a33e4a4394436f9e67c0669660b
DCtheTall <dylancutler@google.com>
2023-04-11 10:18:04 -0400

    Remove artifacts of CHIPS origin trial

--- www/cookies/v2/navigation.php
+++ www/cookies/v2/navigation.php
@@ -1,6 +1,5 @@
 <?php
 header('Set-Cookie: __Host-navigation=foobar; Secure; Path=/; SameSite=None; Partitioned; Max-Age=86400');
-header('Origin-Trial: Ai9tuLs6oePbb8wGl0/CwIcLxDaTQwKf6gGHicjWjvnkLSjmD+4s2KZ/icjDLx/OWb9ql0KXmTXhjbO8m4Q2HAkAAABbeyJvcmlnaW4iOiJodHRwczovL2NyMi5rdW5nZm9vLm5ldDo0NDMiLCJmZWF0dXJlIjoiUGFydGl0aW9uZWRDb29raWVzIiwiZXhwaXJ5IjoxNjU1MjUxMTk5fQ==');
 ?>
 <!DOCTYPE html>
 <meta charset="utf-8">
--- www/cookies/v2/opt-out.php
+++
@@ -1,3 +0,0 @@
-<?php
-header('Set-Cookie: __Host-shouldnotrender=foobar; Secure; Path=/; SameSite=None; Partitioned; Max-Age=0');
-?>
