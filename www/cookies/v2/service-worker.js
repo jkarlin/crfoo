72370b12197680110c50624597ca04588fdbd677
DCtheTall <dylancutler@google.com>
2023-04-11 11:32:04 -0400

    Add cookie change listener to service worker on cookie test site

--- www/cookies/v2/service-worker.js
+++ www/cookies/v2/service-worker.js
@@ -2,4 +2,13 @@
  * Service worker script executes in the worker.
  */
 
-console.log('Hello, world! I am a nascient worker!');
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const subscriptions = await self.registration.cookies.getSubscriptions();
+    await self.registration.cookies.unsubscribe(subscriptions);
+
+    await self.registration.cookies.subscribe([{url: self.registration.scope}]);
+  })());
+});
+
+self.addEventListener('cookiechange', ev => console.log('Cookie change detected', ev));
