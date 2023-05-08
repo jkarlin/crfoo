c372043c3f0cac67b1567e60ae3d93844b0a17f2
Yao Xiao <xyaof3@126.com>
2023-05-08 18:03:15 -0400

    Update observe_browsing_topics3.php

--- www/yao/browsing_topics/observe_browsing_topics3.php
+++ www/yao/browsing_topics/observe_browsing_topics3.php
@@ -1,7 +1,8 @@
 <?php
 header("Observe-Browsing-Topics: 1");
 foreach (getallheaders() as $name => $value) {
-    echo "$name: strlen($value)\n";
+    echo "$name:";
+    echo strlen($value);
 }
 ?>
 <body>
