219cddcb6d8218320e37562df2b286093862176e
Yao Xiao <xyaof3@126.com>
2023-07-31 13:15:42 -0400

    Create shared_worker.js

---
+++ www/yao/shared_storage/shared_worker.js
@@ -0,0 +1,15 @@
+const allPorts = [];
+
+onconnect = function(e) {
+  var port = e.ports[0];
+  allPorts.push(port);
+
+  port.addEventListener('message', function(e) {
+    var message = e.data[0];
+    allPorts.forEach(port => {
+      port.postMessage(message);
+    })
+  });
+
+  port.start();
+}
