4ccc727b9fa4f0d87752851852455b89c23ac511
Yao Xiao <xyaof3@126.com>
2023-08-03 12:45:38 -0400

    Create module_with_breakpoint.js

---
+++ www/yao/shared_storage/module_with_breakpoint.js
@@ -0,0 +1,22 @@
+debugger;
+
+class TestSetOperation {
+  async run(data) {
+    console.log("====== TestSetOperation started =====");
+    await sharedStorage.set('key0', 'value0');
+    console.log("====== TestSetOperation finished =====");
+    return 1;
+  }
+}
+
+class TestGetOperation {
+  async run(data) {
+    console.log("====== TestGetOperation started =====");
+    let v = await sharedStorage.get('key0');
+    console.log(v);
+    console.log("====== TestGetOperation finished =====");
+  }
+}
+
+register("test-set-operation", TestSetOperation);
+register("test-get-operation", TestGetOperation);
