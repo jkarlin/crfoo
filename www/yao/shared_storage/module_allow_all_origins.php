10a1919f712a82eb29a79f62a49cf3c3070c6373
Yao Xiao <xyaof3@126.com>
2023-08-30 15:25:23 -0400

    Update module_allow_all_origins.php

--- www/yao/shared_storage/module_allow_all_origins.php
+++ www/yao/shared_storage/module_allow_all_origins.php
@@ -1,4 +1,5 @@
 <?php
+$path = 'module.js';
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/javascript");
-?>
+header('Content-Type: application/javascript');
+readfile($path);
