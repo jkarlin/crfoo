29dd4005818a98af822dcf36a6bb484fd2dd8879
Yao Xiao <xyaof3@126.com>
2023-09-08 15:34:56 -0400

    Update sw.js

--- www/yao/downloads/sw.js
+++ www/yao/downloads/sw.js
@@ -6,6 +6,5 @@ self.addEventListener("install", () => {
 
 self.addEventListener("message", async (e) => {
     let clients = await self.clients.matchAll({ includeUncontrolled: true, type: "window" });
-    // clients[0].navigate("https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-zip-file.zip");
-    clients[0].navigate("https://www.google.com");
+    clients[0].navigate("https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-zip-file.zip");
 });
