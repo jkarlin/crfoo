aafa41eb98bf633aca9df7b6a38d3534e634d2ff
Yao Xiao <xyaof3@126.com>
2023-09-19 15:10:39 -0400

    Update module.js

--- www/yao/shared_storage/module.js
+++ www/yao/shared_storage/module.js
@@ -1,23 +1,2 @@
-class TestSetOperation {
-  async run(data) {
-    console.log("====== TestSetOperation started =====");
-    await sharedStorage.set('key0', 'value0');
-    console.log("====== TestSetOperation finished =====");
-    return 1;
-  }
-}
 
-let a = 0;
 
-class TestGetOperation {
-  async run(data) {
-    debugger;
-    console.log("====== TestGetOperation started =====");
-    let v = await sharedStorage.get('key0');
-    console.log(v);
-    console.log("====== TestGetOperation finished =====");
-  }
-}
-
-register("test-set-operation", TestSetOperation);
-register("test-get-operation", TestGetOperation);
