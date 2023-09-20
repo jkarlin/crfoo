d3006573b6ddc9437d4beb38ac815407308c5989
DCtheTall <dylancutler@google.com>
2023-09-20 17:15:02 -0400

    Simplify video endpoint to test

--- www/cookies/v2/video.php
+++ www/cookies/v2/video.php
@@ -1,18 +1,19 @@
 <?php
-$file = 'bananas.mp4'
 try {
+    $file = 'bananas.mp4'
     if (file_exists($file)) {
-        header('Content-Description: File Transfer');
-        header('Content-Type: video/mp4');
-        header('Content-Length: ' . filesize($file));
-        header('Content-Security-Policy: sandbox');
-        readfile($file);
-        http_response_code(200);
+        echo 'File does exist'
+        // header('Content-Description: File Transfer');
+        // header('Content-Type: video/mp4');
+        // header('Content-Length: ' . filesize($file));
+        // header('Content-Security-Policy: sandbox');
+        // readfile($file);
+        // http_response_code(200);
     } else {
-        http_response_code(404);
+        // http_response_code(404);
+        echo 'File does not exist'
     }
 } catch (Exception $e) {
-    http_response_code(500);
     echo 'Caught exception: ',  $e->getMessage(), '\n';
 }
 ?>
