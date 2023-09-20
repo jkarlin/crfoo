efd3e399c85c0b8f7dfc5015863ba17c93e0712a
DCtheTall <dylancutler@google.com>
2023-09-20 17:07:37 -0400

    Use single quotes

--- www/cookies/v2/video.php
+++ www/cookies/v2/video.php
@@ -1,16 +1,13 @@
 <?php
-
-$file = "static/bananas.mp4"
-
+$file = 'bananas.mp4'
 if (file_exists($file)) {
-    header("Content-Description: File Transfer");
-    header("Content-Type: video/mp4");
-    header("Content-Length: " . filesize($file));
-    header("Content-Security-Policy: sandbox");
+    header('Content-Description: File Transfer');
+    header('Content-Type: video/mp4');
+    header('Content-Length: ' . filesize($file));
+    header('Content-Security-Policy: sandbox');
     readfile($file);
     exit;  
 } else {
     http_response_code(404);
 }
-
 ?>
