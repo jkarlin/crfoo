2892647f0c7fda2be208af082bc8813d2033fa3e
DCtheTall <dylancutler@google.com>
2023-09-20 17:01:20 -0400

    Add endpoint to serve video with custom CSP header

---
+++ www/cookies/v2/video.php
@@ -0,0 +1,14 @@
+<?php
+
+$file = "./static/bananas.mp4"
+
+if (file_exists($file)) {
+    header("Content-Description: File Transfer");
+    header("Content-Type: video/mp4");
+    header("Content-Length: " . filesize($file));
+    header("Content-Security-Policy: sandbox");
+    readfile($file);
+    exit;  
+}
+
+?>
