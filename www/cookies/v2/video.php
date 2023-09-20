9f5ebb26805b2299bd1844ca645d7efdab452314
DCtheTall <dylancutler@google.com>
2023-09-20 17:11:18 -0400

    Add try/catch

--- www/cookies/v2/video.php
+++ www/cookies/v2/video.php
@@ -1,13 +1,18 @@
 <?php
 $file = 'bananas.mp4'
-if (file_exists($file)) {
-    header('Content-Description: File Transfer');
-    header('Content-Type: video/mp4');
-    header('Content-Length: ' . filesize($file));
-    header('Content-Security-Policy: sandbox');
-    readfile($file);
-    exit;  
-} else {
-    http_response_code(404);
+try {
+    if (file_exists($file)) {
+        header('Content-Description: File Transfer');
+        header('Content-Type: video/mp4');
+        header('Content-Length: ' . filesize($file));
+        header('Content-Security-Policy: sandbox');
+        readfile($file);
+        http_response_code(200);
+    } else {
+        http_response_code(404);
+    }
+} catch (Exception $e) {
+    http_response_code(500);
+    echo 'Caught exception: ',  $e->getMessage(), '\n';
 }
 ?>
