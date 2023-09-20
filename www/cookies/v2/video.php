bc65a602d3ee40edf1dc05f82984937458b4174d
DCtheTall <dylancutler@google.com>
2023-09-20 17:34:55 -0400

    Do not serve video without a cookie

--- www/cookies/v2/video.php
+++ www/cookies/v2/video.php
@@ -1,12 +1,16 @@
 <?php
 try {
+    header('Content-Type: video/mp4');
+    header('Content-Description: File Transfer');
+    header('Cache-Control: no-cache, must-revalidate');
+    header('Content-Security-Policy: sandbox allow-scripts');
     $file = 'bananas.mp4';
-    if (file_exists($file)) {
-        header('Content-Description: File Transfer');
-        header('Content-Type: video/mp4');
+    $cookie_name = 'unpartitioned';
+    if (!isset($_COOKIE[$cookie_name])) {
+        http_status_code(401);
+        echo 'No cookie!\n';
+    } elseif (file_exists($file)) {
         header('Content-Length: ' . filesize($file));
-        header('Content-Security-Policy: sandbox allow-scripts');
-        header('Cache-Control: no-cache, must-revalidate');
         http_response_code(200);
         readfile($file);
     } else {
