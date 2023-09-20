47826bbc84dbea95cbf6de17a413d57143dc0fae
DCtheTall <dylancutler@google.com>
2023-09-20 17:21:56 -0400

    Add missing semicolon :P

--- www/cookies/v2/video.php
+++ www/cookies/v2/video.php
@@ -1,17 +1,16 @@
 <?php
 try {
-    $file = 'bananas.mp4'
+    $file = 'bananas.mp4';
     if (file_exists($file)) {
-        echo 'File does exist'
-        // header('Content-Description: File Transfer');
-        // header('Content-Type: video/mp4');
-        // header('Content-Length: ' . filesize($file));
-        // header('Content-Security-Policy: sandbox');
-        // readfile($file);
-        // http_response_code(200);
+        header('Content-Description: File Transfer');
+        header('Content-Type: video/mp4');
+        header('Content-Length: ' . filesize($file));
+        header('Content-Security-Policy: sandbox');
+        http_response_code(200);
+        readfile($file);
     } else {
-        // http_response_code(404);
-        echo 'File does not exist'
+        http_response_code(404);
+        echo 'File does not exist\n';
     }
 } catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), '\n';
