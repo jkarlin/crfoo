c1f76eebc35526c10a9c3717b7dcbbae9b11a125
Yao Xiao <xyaof3@126.com>
2023-09-27 11:29:50 -0400

    Create module3.js

---
+++ www/yao/shared_storage/module3.js
@@ -0,0 +1,28 @@
+function add(a, b) { 
+  return a + b;
+}
+
+let sum = 0;
+
+for (let i = 0; i < 100000; ++i) {
+  sum += add(i, i+1)
+}
+
+class TestSetOperation {
+  async run(data) {
+    console.log("====== TestSetOperation started =====");
+    console.log(sum);
+    console.log("====== TestSetOperation finished =====");
+  }
+}
+
+class TestGetOperation {
+  async run(data) {
+    console.log("====== TestSetOperation started =====");
+    console.log(sum);
+    console.log("====== TestSetOperation finished =====");
+  }
+}
+
+register("test-set-operation", TestSetOperation);
+register("test-get-operation", TestGetOperation);
