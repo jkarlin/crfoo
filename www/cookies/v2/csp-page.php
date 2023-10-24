454b7a129cdd9ad454e7b4a352b71daf1ade63dd
DCtheTall <dylancutler@google.com>
2023-10-24 09:11:17 -0400

    Add more text to CSP cookie page

--- www/cookies/v2/csp-page.php
+++ www/cookies/v2/csp-page.php
@@ -3,6 +3,8 @@ header('Content-Security-Policy: sandbox allow-scripts');
 ?>
 <!DOCTYPE html>
 <meta charset="utf-8">
-<video src="./video.php" width="500px"></video>
+<video src="./video.php" width="500px" onerror="alert('Video will not load');"></video>
 <br>
 The video will when the "unpartitioned" cookie is available and will not load if the cookie is blocked.
+<br>
+The video may take time to load, so check the Network tab in DevTools for the result of the request more immediately.
