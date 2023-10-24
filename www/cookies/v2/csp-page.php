1c954b10d1d15aa394119592aacce6b226764359
DCtheTall <dylancutler@google.com>
2023-10-24 09:03:44 -0400

    Add page on cookie test page with CSP

---
+++ www/cookies/v2/csp-page.php
@@ -0,0 +1,8 @@
+<?php
+header('Content-Security-Policy: sandbox allow-scripts');
+?>
+<!DOCTYPE html>
+<meta charset="utf-8">
+<video src="./video.php" width="500px">
+<br>
+The video will when the "unpartitioned" cookie is available and will not load if the cookie is blocked.
