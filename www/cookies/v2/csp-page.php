56a7f161b2adadf991046fde910d44fe4fd9731d
DCtheTall <dylancutler@google.com>
2023-10-24 09:08:36 -0400

    Close <video>

--- www/cookies/v2/csp-page.php
+++ www/cookies/v2/csp-page.php
@@ -3,6 +3,6 @@ header('Content-Security-Policy: sandbox allow-scripts');
 ?>
 <!DOCTYPE html>
 <meta charset="utf-8">
-<video src="./video.php" width="500px">
+<video src="./video.php" width="500px"></video>
 <br>
 The video will when the "unpartitioned" cookie is available and will not load if the cookie is blocked.
