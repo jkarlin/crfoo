fbad2f4e6cf9493508b43099c9aef3511d0520b0
Yao Xiao <xyaof3@126.com>
2023-10-30 15:07:28 -0400

    Update simple.php

--- www/yao/shared_storage/simple.php
+++ www/yao/shared_storage/simple.php
@@ -2,4 +2,5 @@
 $path = 'simple.html';
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: text/html; charset=utf-8');
+header('Set-Cookie: qweasd=abc123; SameSite=None');
 readfile($path);
