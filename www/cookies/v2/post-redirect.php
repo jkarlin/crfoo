7c3e232d8af9f4b35e6614b9642f737a22d3bd8b
Dylan Cutler <dylancutler@google.com>
2023-11-14 16:19:41 -0500

    Temporarily make POST redirect not redirect

--- www/cookies/v2/post-redirect.php
+++ www/cookies/v2/post-redirect.php
@@ -1,10 +1,11 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  header(
-      'Location: ' . $_POST['location'] . '?cookie=' . getallheaders()['Cookie']);
-  http_response_code(301);
-} else {
-  http_response_code(404);
-}
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//   header(
+//       'Location: ' . $_POST['location'] . '?cookie=' . getallheaders()['Cookie']);
+//   http_response_code(301);
+// } else {
+//   http_response_code(404);
+// }
+http_response_code(200);
 ?>
