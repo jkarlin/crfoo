042f14891e9bbd934e59d43373e586eaf8a5c199
Josh Karlin <jkarlin@users.noreply.github.com>
2023-12-21 09:56:16 -0500

    Update worklet.js

--- www/jkarlin/su/worklet.js
+++ www/jkarlin/su/worklet.js
@@ -1,11 +1,16 @@
-class SelectURLOperation {
+class SendReport {
   async run(urls, data) {
     let j = 0;
     for(let i = 0; i < 10000000; i++) {
       j += 1;
     }
-    return 1;
+
+    // The user agent will send the report to a default endpoint after a delay.
+    privateAggregation.sendHistogramReport({
+      bucket: 0,
+      value: 128, // A predetermined fixed value; see Private Aggregation API explainer: Scaling values.
+    });
   }
 }
 
-register('foo', SelectURLOperation);
+register('foo', SendReport);
