c8a368cfb7c3b0d7052029a383b801aa0b6bc989
Dylan Cutler <dylancutler@google.com>
2023-12-21 10:44:43 -0500

    Add SameSite=None CORS endpoint to test cookie site

---
+++ www/cookies/v2/samesitenone-cors.php
@@ -0,0 +1,5 @@
+<?php
+header('Access-Control-Allow-Origin: https://wobbly-violet-heaven.glitch.me');
+header('Access-Control-Allow-Credentials: true');
+header('Set-Cookie: unpartitioned=foobar; Secure; Path=/; SameSite=None; Max-Age=86400');
+?>
