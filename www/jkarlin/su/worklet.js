348b495eca9d490769588bbc0d4d15f6733f0d6e
Josh Karlin <jkarlin@users.noreply.github.com>
2023-12-21 09:48:10 -0500

    Update worklet.js

--- www/jkarlin/su/worklet.js
+++ www/jkarlin/su/worklet.js
@@ -1,7 +1,7 @@
 class SelectURLOperation {
   async run(urls, data) {
     let j = 0;
-    for(i = 0; i < 10000000; i++) {
+    for(let i = 0; i < 10000000; i++) {
       j += 1;
     }
     return 1;
