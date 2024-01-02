a4e68f24b23cf74ecd2c3a9b0b83f875f4c3b2b7
Camillia Smith Barnes <cammie@chromium.org>
2024-01-02 14:14:41 -0500

    Update index.php

--- www/cammie/cookies/index.php
+++ www/cammie/cookies/index.php
@@ -1,8 +1,11 @@
 <?php
+header('Origin-Trial: Ai9tuLs6oePbb8wGl0/CwIcLxDaTQwKf6gGHicjWjvnkLSjmD+4s2KZ/icjDLx/OWb9ql0KXmTXhjbO8m4Q2HAkAAABbeyJvcmlnaW4iOiJodHRwczovL2NyMi5rdW5nZm9vLm5ldDo0NDMiLCJmZWF0dXJlIjoiUGFydGl0aW9uZWRDb29raWVzIiwiZXhwaXJ5IjoxNjU1MjUxMTk5fQ==');
+header('Accept-CH: Sec-CH-Partitioned-Cookies');
 header("Access-Control-Allow-Origin: *");
 ?>
 <!DOCTYPE html>
 <meta charset="utf-8">
+<meta http-equiv="origin-trial" content="Ai9tuLs6oePbb8wGl0/CwIcLxDaTQwKf6gGHicjWjvnkLSjmD+4s2KZ/icjDLx/OWb9ql0KXmTXhjbO8m4Q2HAkAAABbeyJvcmlnaW4iOiJodHRwczovL2NyMi5rdW5nZm9vLm5ldDo0NDMiLCJmZWF0dXJlIjoiUGFydGl0aW9uZWRDb29raWVzIiwiZXhwaXJ5IjoxNjU1MjUxMTk5fQ==">
 <button id="set-cookie">Set cookie</button>
 <button id="set-cookie-from-cache">Set cookie from cache</button>
 <div>document.cookie is <span id="document-cookie"></span></div>
