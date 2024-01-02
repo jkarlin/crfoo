fa7a554cedc36d9514cf14a5ae62b2e09dcb0e47
Camillia Smith Barnes <cammie@chromium.org>
2024-01-02 14:42:54 -0500

    Update set-cookie.php

--- www/cammie/cookies/set-cookie.php
+++ www/cammie/cookies/set-cookie.php
@@ -1,5 +1,6 @@
 <?php
-header('Set-Cookie: unpartitioned=foobar; Secure; Path=/; SameSite=None; Max-Age=86400');
+header('Cache-Control: max-age=3600');
+header('Set-Cookie: unpartitioned=foobar; Secure; Path=/; SameSite=None; Max-Age=300');
 header('Origin-Trial: Ai9tuLs6oePbb8wGl0/CwIcLxDaTQwKf6gGHicjWjvnkLSjmD+4s2KZ/icjDLx/OWb9ql0KXmTXhjbO8m4Q2HAkAAABbeyJvcmlnaW4iOiJodHRwczovL2NyMi5rdW5nZm9vLm5ldDo0NDMiLCJmZWF0dXJlIjoiUGFydGl0aW9uZWRDb29raWVzIiwiZXhwaXJ5IjoxNjU1MjUxMTk5fQ==');
 header('Accept-CH: Sec-CH-Partitioned-Cookies');
 ?>
