7a35b208a9bf49d0cb26d19d7411ce4fff04b5fe
DCtheTall <dylancutler@google.com>
2024-01-16 20:05:19 -0500

    Add CORS headers to 3P cookie endpoint

--- www/cookies/v2/samesitenone.php
+++ www/cookies/v2/samesitenone.php
@@ -1,4 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: null');
+header('Access-Control-Allow-Credentials: true');
 header('Set-Cookie: unpartitioned=foobar; Secure; Path=/; SameSite=None; Max-Age=86400');
 ?>
