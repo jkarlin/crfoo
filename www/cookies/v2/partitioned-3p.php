818cd0632b2f31188928b7cf187ef2d06ea34ff1
DCtheTall <dylancutler@google.com>
2024-01-16 19:37:01 -0500

    Add Access-Control-Allow-Credentials to 3P partitioned cookie endpoint

--- www/cookies/v2/partitioned-3p.php
+++ www/cookies/v2/partitioned-3p.php
@@ -1,5 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Origin: null');
+header('Access-Control-Allow-Credentials: true');
 header('Set-Cookie: __Host-3P_partitioned=foobar; Secure; Path=/; SameSite=None; Partitioned; Max-Age=86400');
 ?>
