990a2a9f28ae9bb4e904d12e02535317ead2493b
DCtheTall <dylancutler@google.com>
2024-01-16 19:33:09 -0500

    Allow null origin to set partitioned 3P cookie

--- www/cookies/v2/partitioned-3p.php
+++ www/cookies/v2/partitioned-3p.php
@@ -1,4 +1,5 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: null');
 header('Set-Cookie: __Host-3P_partitioned=foobar; Secure; Path=/; SameSite=None; Partitioned; Max-Age=86400');
 ?>
