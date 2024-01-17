9dd434bad6a2cea46f28c1bcdf45e6d7036d2d18
DCtheTall <dylancutler@google.com>
2024-01-17 10:10:06 -0500

    Add CORS headers to CSD endpoints

--- www/cookies/v2/clear-site-data-cookies.php
+++ www/cookies/v2/clear-site-data-cookies.php
@@ -1,4 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: null');
+header('Access-Control-Allow-Credentials: true');
 header('Clear-Site-Data: "cookies"');
 ?>
