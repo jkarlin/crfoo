88e1e05f036dda0df189ff6f3a02df2f28d03179
Charlie Harrison <csharrison@chromium.org>
2024-01-19 10:14:41 -0600

    update sf urls

--- www/subresource_filter/ads_in_workers/shared_worker_fetch.js
+++ www/subresource_filter/ads_in_workers/shared_worker_fetch.js
@@ -1,6 +1,6 @@
 onconnect = function(e) {
   var port = e.ports[0];
-  var src = '../social-engineering-ad1.png?from_shared_worker';
+  var src = '../social-engineering-ad1.png?from_shared_worker&/adservices.';
   fetch(src).then((response) => {
     response.blob().then((r) => port.postMessage(r));
   },
--- www/subresource_filter/ads_in_workers/worker_fetch.js
+++ www/subresource_filter/ads_in_workers/worker_fetch.js
@@ -1,4 +1,4 @@
-var src="../social-engineering-ad1.png?from_dedicated_worker";
+var src="../social-engineering-ad1.png?from_dedicated_worker&/adservices.";
 
 fetch(src).then((response) => {
   response.blob().then((r) => postMessage(r));
