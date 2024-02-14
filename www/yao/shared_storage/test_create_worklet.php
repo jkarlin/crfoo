778900f8f37a1649574293acec0e31071966c526
Yao Xiao <xyaof3@126.com>
2024-02-13 23:09:28 -0500

    Update test_create_worklet.php

--- www/yao/shared_storage/test_create_worklet.php
+++ www/yao/shared_storage/test_create_worklet.php
@@ -16,12 +16,18 @@ header('Cross-Origin-Embedder-Policy: require-corp');
       async function createWorklet(src) {
         const worklet = await sharedStorage.createWorklet(src, { credentials: "include" });
       }
+
+      async function registerServiceWorker(src) {
+        const registration = await navigator.serviceWorker.register(src);
+      }
     </script>
 
     <button onclick="createWorklet('https://cr.kungfoo.net/yao/shared_storage/module.js')">createWorklet() same-origin</button><br>
     <button onclick="appendIframe('https://cr.kungfoo.net/yao/shared_storage/iframe.html')">appendChild() same-origin</button><br>
     <button onclick="createWorklet('https://ss-create-worklet.glitch.me/module.js')">createWorklet() cross-origin</button><br>
     <button onclick="appendIframe('https://ss-create-worklet.glitch.me/iframe.html')">appendChild() cross-origin</button><br>
+    
+    <button onclick="registerServiceWorker('https://cr.kungfoo.net/yao/shared_storage/sw.js')">registerServiceWorker()</button><br>
 
   </body>
 </html>
