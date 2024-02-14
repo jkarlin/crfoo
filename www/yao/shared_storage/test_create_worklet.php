f9422f34929e4c4a8d4d953146b58538661562f8
Yao Xiao <xyaof3@126.com>
2024-02-13 23:17:46 -0500

    Update test_create_worklet.php

--- www/yao/shared_storage/test_create_worklet.php
+++ www/yao/shared_storage/test_create_worklet.php
@@ -1,8 +1,3 @@
-<?php
-header('Content-Type: text/html; charset=utf-8');
-header('Cross-Origin-Opener-Policy: same-origin');
-header('Cross-Origin-Embedder-Policy: require-corp');
-?>
 <!DOCTYPE html>
 <html lang="en">
   <body>
