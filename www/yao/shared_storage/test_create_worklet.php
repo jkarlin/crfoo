da95480720cdf045fa52cf636b0a835ce5770a5c
Yao Xiao <xyaof3@126.com>
2024-02-14 12:06:22 -0500

    Create test_create_worklet.php

---
+++ www/yao/shared_storage/test_create_worklet.php
@@ -0,0 +1,33 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+header('Cross-Origin-Opener-Policy: same-origin');
+header('Cross-Origin-Embedder-Policy: require-corp');
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <body>
+    <script>
+      function appendIframe(src) {
+        const iframe = document.createElement("iframe");
+        iframe.src = src;
+        document.body.appendChild(iframe);
+      }
+      
+      async function createWorklet(src) {
+        const worklet = await sharedStorage.createWorklet(src, { credentials: "include" });
+      }
+
+      async function registerServiceWorker(src) {
+        const registration = await navigator.serviceWorker.register(src);
+      }
+    </script>
+
+    <button onclick="createWorklet('https://cr.kungfoo.net/yao/shared_storage/module.js')">createWorklet() same-origin</button><br>
+    <button onclick="appendIframe('https://cr.kungfoo.net/yao/shared_storage/empty.html')">appendChild() same-origin</button><br>
+    <button onclick="createWorklet('https://ss-create-worklet.glitch.me/module.js')">createWorklet() cross-origin</button><br>
+    <button onclick="appendIframe('https://ss-create-worklet.glitch.me/iframe.html')">appendChild() cross-origin</button><br>
+    
+    <button onclick="registerServiceWorker('https://cr.kungfoo.net/yao/shared_storage/sw.php')">registerServiceWorker()</button><br>
+
+  </body>
+</html>
