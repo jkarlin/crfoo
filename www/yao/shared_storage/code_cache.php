6046950429740d2d7932bc01294f11181887e1f6
Yao Xiao <xyaof3@126.com>
2024-05-09 17:57:14 -0400

    Update code_cache.php

--- www/yao/shared_storage/code_cache.php
+++ www/yao/shared_storage/code_cache.php
@@ -1,4 +1,6 @@
 <?php
+$path = 'large-script.js';
 header('Content-Type: application/javascript');
-header("Cache-Control: max-age=2592000, must-revalidate");
+header("Cache-Control: max-age=2592000");
+readfile($path);
 ?>
