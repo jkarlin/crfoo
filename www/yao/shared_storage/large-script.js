d284c6bcac4c170b158030d8ea57a16bdcd52305
Yao Xiao <xyaof3@126.com>
2024-05-09 17:59:46 -0400

    Create large-script.js

---
+++ www/yao/shared_storage/large-script.js
@@ -0,0 +1,164 @@
+class TestURLSelectionOperation {
+  async run(urls, data) {
+    if (data && data.hasOwnProperty('setKey') && data.hasOwnProperty('setValue')) {
+      await sharedStorage.set(data['setKey'], data['setValue']);
+    }
+
+    if (data && data.hasOwnProperty('mockResult')) {
+      return data['mockResult'];
+    }
+
+    return -1;
+  }
+}
+
+
+class TestURLSelectionOperation2 {
+  async run(urls, data) {
+    if (data && data.hasOwnProperty('setKey') && data.hasOwnProperty('setValue')) {
+      await sharedStorage.set(data['setKey'], data['setValue']);
+    }
+
+    if (data && data.hasOwnProperty('mockResult')) {
+      return data['mockResult'];
+    }
+
+    return -1;
+  }
+}
+
+class TestURLSelectionOperation3 {
+  async run(urls, data) {
+    if (data && data.hasOwnProperty('setKey') && data.hasOwnProperty('setValue')) {
+      await sharedStorage.set(data['setKey'], data['setValue']);
+    }
+
+    if (data && data.hasOwnProperty('mockResultt')) {
+      return data['mockResult'];
+    }
+
+    return -1;
+  }
+}
+
+class TestURLSelectionOperation4 {
+  async run(urls, data) {
+    if (data && data.hasOwnProperty('setKeytt') && data.hasOwnProperty('setValue')) {
+      await sharedStorage.set(data['setKey'], data['setValue']);
+    }
+
+    if (data && data.hasOwnProperty('mockResult')) {
+      return data['mockResult'];
+    }
+
+    return -1;
+  }
+}
+
+class TestURLSelectionOperation5 {
+  async run(urls, data) {
+    if (data && data.hasOwnProperty('setKey') && data.hasOwnProperty('setValue')) {
+      await sharedStorage.set(data['setKey'], data['setValue']);
+    }
+
+    if (data && data.hasOwnProperty('mockResult')) {
+      return data['mockResult'];
+    }
+
+    return -1;
+  }
+}
+
+class TestURLSelectionOperation6 {
+  async run(urls, data) {
+    if (data && data.hasOwnProperty('setKeytt') && data.hasOwnProperty('setValue')) {
+      await sharedStorage.set(data['setKey'], data['setValue']);
+    }
+
+    if (data && data.hasOwnProperty('mockResult')) {
+      return data['mockResult'];
+    }
+
+    return -1;
+  }
+}
+
+class TestURLSelectionOperation7 {
+  async run(urls, data) {
+    if (data && data.hasOwnProperty('setKey') && data.hasOwnProperty('setValue')) {
+      await sharedStorage.set(data['setKey'], data['setValue']);
+    }
+
+    if (data && data.hasOwnProperty('mockResult')) {
+      return data['mockResult'];
+    }
+
+    return -1;
+  }
+}
+
+class TestURLSelectionOperation8 {
+  async run(urls, data) {
+    if (data && data.hasOwnProperty('setKeytt') && data.hasOwnProperty('setValue')) {
+      await sharedStorage.set(data['setKey'], data['setValue']);
+    }
+
+    if (data && data.hasOwnProperty('mockResult')) {
+      return data['mockResult'];
+    }
+
+    return -1;
+  }
+}
+
+class TestURLSelectionOperation9 {
+  async run(urls, data) {
+    if (data && data.hasOwnProperty('setKey') && data.hasOwnProperty('setValue')) {
+      await sharedStorage.set(data['setKey'], data['setValue']);
+    }
+
+    if (data && data.hasOwnProperty('mockResult')) {
+      return data['mockResult'];
+    }
+
+    return -1;
+  }
+}
+
+class TestURLSelectionOperation10 {
+  async run(urls, data) {
+    if (data && data.hasOwnProperty('setKeytt') && data.hasOwnProperty('setValue')) {
+      await sharedStorage.set(data['setKey'], data['setValue']);
+    }
+
+    if (data && data.hasOwnProperty('mockResult')) {
+      return data['mockResult'];
+    }
+
+    return -1;
+  }
+}
+
+class TestURLSelectionOperation11 {
+  async run(urls, data) {
+    if (data && data.hasOwnProperty('setKey') && data.hasOwnProperty('setValue')) {
+      await sharedStorage.set(data['setKey'], data['setValue']);
+    }
+
+    if (data && data.hasOwnProperty('mockResult')) {
+      return data['mockResult'];
+    }
+
+    return -1;
+  }
+}
+
+register('test-url-selection-operation', TestURLSelectionOperation);
+register('test-url-selection-operation2', TestURLSelectionOperation2);
+register('test-url-selection-operation3', TestURLSelectionOperation3);
+register('test-url-selection-operation4', TestURLSelectionOperation4);
+register('test-url-selection-operation6', TestURLSelectionOperation6);
+register('test-url-selection-operation7', TestURLSelectionOperation7);
+register('test-url-selection-operation8', TestURLSelectionOperation8);
+register('test-url-selection-operation9', TestURLSelectionOperation9);
+register('test-url-selection-operation10', TestURLSelectionOperation10);
