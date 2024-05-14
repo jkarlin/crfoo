e2125bd3012bd6ffdcb4e8e584291bf3179c9a3b
Yao Xiao <xyaof3@126.com>
2024-05-14 12:51:23 -0400

    Update module-allow-demo-origin.php

--- www/yao/shared_storage/module-allow-demo-origin.php
+++ www/yao/shared_storage/module-allow-demo-origin.php
@@ -1,5 +1,6 @@
 <?php
 $path = 'demo-module.js';
 header("Access-Control-Allow-Origin: https://shared-storage-cross-origin-worklet-demo.glitch.me");
+header("Shared-Storage-Cross-Origin-Worklet-Allowed: ?1");
 header('Content-Type: application/javascript');
 readfile($path);
