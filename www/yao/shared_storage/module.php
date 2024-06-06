f567f1a85f96af73bd4f478f6efb134fa42806d9
Yao Xiao <xyaof3@126.com>
2024-06-06 17:54:31 -0400

    Update module.php

--- www/yao/shared_storage/module.php
+++ www/yao/shared_storage/module.php
@@ -1,4 +1,6 @@
 <?php
+$path = 'module.js';
+header("Access-Control-Allow-Origin: *");
+header("Shared-Storage-Cross-Origin-Worklet-Allowed: ?1");
 header('Content-Type: application/javascript');
-header('Access-Control-Allow-Origin: *');
-?>
+readfile($path);
