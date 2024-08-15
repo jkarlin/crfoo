5ce572439bc04bbf0432987e8338c011fe13f854
Dylan Cutler <dylancutler@google.com>
2024-08-15 19:38:54 -0400

    Add new endpoint for setting 3P partitioned cookie from top-level frame

---
+++ www/cookies/v2/partitioned-3p-from-toplevel.php
@@ -0,0 +1,5 @@
+<?php
+header('Access-Control-Allow-Origin: https://wobbly-violet-heaven.glitch.me');
+header('Access-Control-Allow-Credentials: true');
+header('Set-Cookie: __Host-3P_partitioned=toplevel; Secure; Path=/; SameSite=None; Partitioned; Max-Age=86400');
+?>
