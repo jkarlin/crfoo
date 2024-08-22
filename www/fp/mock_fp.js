d0c75461c13357b96016dc45d5d5b6cf945e2e86
Dylan Cutler <dylancutler@google.com>
2024-08-22 12:55:00 -0400

    Forgot new keyword for URLSearchParams

--- www/fp/mock_fp.js
+++ www/fp/mock_fp.js
@@ -2,12 +2,12 @@
 (() => {
 
 let fp;
-if (URLSearchParams(location.search).get('a') == '0') {
+if (new URLSearchParams(location.search).get('a') == '0') {
   const x = navigator.language;
   const y = history.length;
   const z = navigator.doNotTrack;
   fp = x + y + z;
-} else if (URLSearchParams(location.search).get('a') == '1') {
+} else if (new URLSearchParams(location.search).get('a') == '1') {
   const x = navigator.deviceMemory;
   const y = navigator.languages;
   const z = navigator.vendor;
