9d870e55716ab32179881927d685bf2836713744
Andrew Liu <liu@chromium.org>
2024-08-27 13:17:12 -0400

    Cross domain bounce tracking with referer header

---
+++ www/mrpickles/http_cache/ajax_bouncer.php
@@ -0,0 +1,27 @@
+<?php
+$referer = $_SERVER["HTTP_REFERER"] ?? "";
+$referer = "'" . addslashes($referer) . "'"
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>AJAX Bounce Tracker</title>
+  </head>
+  <body>
+    <p>This page does an AJAX request for an ETag tracking ID and then client-side redirects back based on the <code>referer</code> header.</p>
+    <p>You should not be seeing this page if all is going as expected.</p>
+    <br />
+    <p><a href="index.html">Back to home page.</a></p>
+    <?php echo "<script>const referer = $referer;</script>"; ?>
+    <script>
+      fetch('ajax.php')
+        .then(res => res.text())
+        .then(body => {
+          const url = new URL(referer);
+          url.searchParams.set('tracking_id', body);
+          url.searchParams.set('tracking_host', window.location.host);
+          window.location.replace(url.toString());
+        });
+    </script>
+  </body>
+</html>
