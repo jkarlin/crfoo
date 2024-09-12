d13c48b555bf7a576dfe6fd7aa00b8e80dfa13ed
Dylan Cutler <dylancutler@google.com>
2024-09-12 14:13:26 -0400

    Fix logging URL in service worker

--- www/cookies/v2/service-worker.js
+++ www/cookies/v2/service-worker.js
@@ -54,5 +54,5 @@ self.addEventListener('message', async event => {
 });
 
 self.addEventListener('fetch', ev => {
-  console.log(`Service worker intercepted request for ${ev.request.fetch}`);
+  console.log(`Service worker intercepted request for ${ev.request.url}`);
 });
