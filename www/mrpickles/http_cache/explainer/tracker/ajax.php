b6d456b348e6d2d1e5ea5fb3241707c4437024f6
Andrew Liu <liu@chromium.org>
2024-10-22 16:44:59 -0400

    Separate out explainer demo

---
+++ www/mrpickles/http_cache/explainer/tracker/ajax.php
@@ -0,0 +1,15 @@
+<?php
+$preexisting_etag = $_SERVER["HTTP_IF_NONE_MATCH"] ?? "";
+$render_time = date("M-j-Y g:i:s A", time());
+
+$etag = $preexisting_etag;
+if (empty($etag)) {
+  $etag = base64_encode(rand() . " :: " . $render_time);
+}
+
+header("Access-Control-Allow-Origin: *");
+header("Cache-Control: no-cache");
+header("ETag: $etag");
+
+echo $etag;
+?>
---
+++ www/mrpickles/http_cache/explainer/tracker/index.php
@@ -0,0 +1,28 @@
+<?php
+$referer = $_GET["bounce_host"] ?? $_SERVER["HTTP_REFERER"] ?? "";
+$referer = "'" . addslashes($referer) . "'"
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>AJAX Bounce Tracker</title>
+  </head>
+  <body>
+    <p>This page does an AJAX request for an ETag tracking ID and then client-side redirects back based on the <code>referer</code> header.</p>
+    <p>You should not be seeing this page if all is going as expected.</p>
+    <br />
+    <p><a href="index.html">Back to home page.</a></p>
+    <?php echo "<script>const referer = $referer;</script>"; ?>
+    <script>
+      fetch('ajax.php')
+        .then(res => res.text())
+        .then(body => {
+          const url = new URL(referer);
+          url.searchParams.set('tracking_id', body);
+          url.searchParams.set('tracking_host', window.location.host);
+          url.searchParams.set('bounce_host', referer);
+          window.location.replace(url.toString());
+        });
+    </script>
+  </body>
+</html>
