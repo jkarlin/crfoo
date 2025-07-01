116e313ae8dde6aa862b80df6877695d6e065f72
Andrew Verge <averge@chromium.org>
2025-07-01 15:28:19 +0000

    Create fenced_frame directory for FF demos

---
+++ www/fenced_frame/index.php
@@ -0,0 +1,20 @@
+<?php
+header("Supports-Loading-Mode: fenced-frame");
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Fenced Frames Demo</title>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+      rel="stylesheet"
+    />
+    <script src=ff_demo.js></script>
+  </head>
+  <body>
+    <h1>Test Commit</h1>
+  </body>
+</html>
