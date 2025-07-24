d7aab49a6da2b07ebda676c40b7beb2bf53cdeb5
Yao Xiao <xyaof3@126.com>
2025-07-24 18:12:44 -0400

    Update regular-script.js

--- www/yao/ad_tagging_promise/regular-script.js
+++ www/yao/ad_tagging_promise/regular-script.js
@@ -1,9 +1,7 @@
 function insertAdFrameFromNonAdScriptAsync(url) {
   Promise.resolve().then(() => {
-    // const iframe = document.createElement('iframe');
-    // iframe.src = url;
-    // document.body.appendChild(iframe);
-
-    insertAdFrameFromAdScript(url);
+    const iframe = document.createElement('iframe');
+    iframe.src = url;
+    document.body.appendChild(iframe);
   });
 }
