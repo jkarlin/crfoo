46bee98ac1d1ceed47fb8f0895f6bf52d3cd5e90
Yao Xiao <xyaof3@126.com>
2025-07-24 15:14:47 -0400

    Update regular-script.js

--- www/yao/ad_tagging_test/regular-script.js
+++ www/yao/ad_tagging_test/regular-script.js
@@ -1,14 +1,10 @@
-function insertAdFrameFromNonAdScript(url) {
-  const iframe = document.createElement('iframe');
-  iframe.src = url;
-  document.body.appendChild(iframe); 
-}
-
 function insertAdFrameFromNonAdScriptAsync(url) {
   sharedStorage.set('a', 'b')
     .then(() => {})
     .catch(error => {
-      insertAdFrameFromNonAdScript(url);
+        const iframe = document.createElement('iframe');
+        iframe.src = url;
+        document.body.appendChild(iframe); 
     });
  
   // setTimeout(() => {
