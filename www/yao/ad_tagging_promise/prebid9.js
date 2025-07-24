4f73dc6cc258f95d78d4ca66c4d5d8600fc22a33
Yao Xiao <xyaof3@126.com>
2025-07-24 18:09:22 -0400

    Update prebid9.js

--- www/yao/ad_tagging_promise/prebid9.js
+++ www/yao/ad_tagging_promise/prebid9.js
@@ -1,8 +1,7 @@
 function insertAdFrameFromAdScript(url) {
-  // const iframe = document.createElement('iframe');
-  // iframe.src = url;
-  // document.body.appendChild(iframe);
-  insertAdFrameFromAdScript(url);
+  const iframe = document.createElement('iframe');
+  iframe.src = url;
+  document.body.appendChild(iframe);
 }
 
 insertAdFrameFromNonAdScriptAsync('https://cdn.glitch.global/cd35de4f-7fd8-4eaf-87f1-9299f60517fc/social-engineering-ad1.png');
