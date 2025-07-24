b2e4791483a0107f744ea3efa3b8d2023d27c90a
Yao Xiao <xyaof3@126.com>
2025-07-24 19:07:31 -0400

    Create regular-script.js

---
+++ www/yao/ad_tagging_promise2/regular-script.js
@@ -0,0 +1,5 @@
+function createAdFrameFromNonAdScript() {
+  const iframe = document.createElement('iframe');
+  iframe.src = 'https://cdn.glitch.global/cd35de4f-7fd8-4eaf-87f1-9299f60517fc/social-engineering-ad1.png';
+  document.body.appendChild(iframe);
+}
