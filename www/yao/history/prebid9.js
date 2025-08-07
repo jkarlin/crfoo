cea5439307be69c541b2858172ffb5a190a61234
Yao Xiao <xyaof3@126.com>
2025-08-07 14:56:41 -0400

    Update prebid9.js

--- www/yao/history/prebid9.js
+++ www/yao/history/prebid9.js
@@ -1,1 +1,9 @@
+function pushStateFromAd(url) {
+  const stateObj = {};
+  const title = "";
+  history.pushState(stateObj, title, url);
+}
 
+function setLocationFromAd(url) {
+  window.location.href = url;
+}
