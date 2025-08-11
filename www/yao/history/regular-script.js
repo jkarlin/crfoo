3b2d809d42004e5b2992e7742703dada85826fce
Yao Xiao <xyaof3@126.com>
2025-08-11 19:05:07 -0400

    Update regular-script.js

--- www/yao/history/regular-script.js
+++ www/yao/history/regular-script.js
@@ -1,9 +1,27 @@
 function pushStateFromRegularScript(url) {
   const stateObj = {};
-  const title = "";
+  const title = "push";
   history.pushState(stateObj, title, url);
 }
 
-function setLocationFromRegularScript(url) {
-  window.location.href = url;
+function replaceStateFromRegularScript(url) {
+  const stateObj = {};
+  const title = "replace";
+  history.replaceState(stateObj, title, url);
+}
+
+function forwardFromRegularScript() {
+  history.forward();
+}
+
+function backFromRegularScript() {
+  history.forward();
+}
+
+function locationAssignFromRegularScript(url) {
+  window.location.assign(url);
+}
+
+function locationReplaceFromRegularScript(url) {
+  window.location.replace(url);
 }
