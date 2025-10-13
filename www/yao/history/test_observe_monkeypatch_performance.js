7fae37c07f40b15e6ea0d983f4d5157ab75699ce
Yao Xiao <xyaof3@126.com>
2025-10-13 13:58:49 -0400

    Update test_observe_monkeypatch_performance.js

--- www/yao/history/test_observe_monkeypatch_performance.js
+++ www/yao/history/test_observe_monkeypatch_performance.js
@@ -1,14 +1,18 @@
 function storeDummyFunctionAndThenMonkeypatch(numIterations) {
+  console.time('storeDummyFunctionAndThenMonkeypatch');
   for (let i = 0; i < numIterations; i++) {
     const originalDummyFunction = history.dummyFunction;
     history.dummyFunction = function(...args) {
       return originalDummyFunction.apply(history, args);
     };
   }
+  console.timeEnd('storeDummyFunctionAndThenMonkeypatch');
 }
 
 function invokeDummyFunction(numIterations) {
+  console.time('invokeDummyFunction');
   for (let i = 0; i < numIterations; i++) {
     history.dummyFunction();
   }
+  console.timeEnd('invokeDummyFunction');
 }
