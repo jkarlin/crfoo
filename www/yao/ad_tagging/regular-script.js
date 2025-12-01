9455168ed7331e4d8a8d981a65d9ef9b90f0f58a
Yao Xiao <xyaof3@126.com>
2025-12-01 17:30:09 -0500

    Update regular-script.js

--- www/yao/ad_tagging/regular-script.js
+++ www/yao/ad_tagging/regular-script.js
@@ -3,7 +3,6 @@ function appendInvisibleIframe(url) {
 }
 
 function layer1(url) {
-    console.log('Entering Layer 1');
     return layer2(url);
 }
 
@@ -28,7 +27,259 @@ function layer6(url) {
 }
 
 function layer7(url) {
-    console.log('Execution Layer Reached: Creating Iframe');
+    return layer8(url);
+}
+
+function layer8(url) {
+    return layer9(url);
+}
+
+function layer9(url) {
+    return layer10(url);
+}
+
+function layer10(url) {
+    return layer11(url);
+}
+
+function layer11(url) {
+    return layer12(url);
+}
+
+function layer12(url) {
+    return layer13(url);
+}
+
+function layer13(url) {
+    return layer14(url);
+}
+
+function layer14(url) {
+    return layer15(url);
+}
+
+function layer15(url) {
+    return layer16(url);
+}
+
+function layer16(url) {
+    return layer17(url);
+}
+
+function layer17(url) {
+    return layer18(url);
+}
+
+function layer18(url) {
+    return layer19(url);
+}
+
+function layer19(url) {
+    return layer20(url);
+}
+
+function layer20(url) {
+    return layer21(url);
+}
+
+function layer21(url) {
+    return layer22(url);
+}
+
+function layer22(url) {
+    return layer23(url);
+}
+
+function layer23(url) {
+    return layer24(url);
+}
+
+function layer24(url) {
+    return layer25(url);
+}
+
+function layer25(url) {
+    return layer26(url);
+}
+
+function layer26(url) {
+    return layer27(url);
+}
+
+function layer27(url) {
+    return layer28(url);
+}
+
+function layer28(url) {
+    return layer29(url);
+}
+
+function layer29(url) {
+    return layer30(url);
+}
+
+function layer30(url) {
+    return layer31(url);
+}
+
+function layer31(url) {
+    return layer32(url);
+}
+
+function layer32(url) {
+    return layer33(url);
+}
+
+function layer33(url) {
+    return layer34(url);
+}
+
+function layer34(url) {
+    return layer35(url);
+}
+
+function layer35(url) {
+    return layer36(url);
+}
+
+function layer36(url) {
+    return layer37(url);
+}
+
+function layer37(url) {
+    return layer38(url);
+}
+
+function layer38(url) {
+    return layer39(url);
+}
+
+function layer39(url) {
+    return layer40(url);
+}
+
+function layer40(url) {
+    return layer41(url);
+}
+
+function layer41(url) {
+    return layer42(url);
+}
+
+function layer42(url) {
+    return layer43(url);
+}
+
+function layer43(url) {
+    return layer44(url);
+}
+
+function layer44(url) {
+    return layer45(url);
+}
+
+function layer45(url) {
+    return layer46(url);
+}
+
+function layer46(url) {
+    return layer47(url);
+}
+
+function layer47(url) {
+    return layer48(url);
+}
+
+function layer48(url) {
+    return layer49(url);
+}
+
+function layer49(url) {
+    return layer50(url);
+}
+
+function layer50(url) {
+    return layer51(url);
+}
+
+function layer51(url) {
+    return layer52(url);
+}
+
+function layer52(url) {
+    return layer53(url);
+}
+
+function layer53(url) {
+    return layer54(url);
+}
+
+function layer54(url) {
+    return layer55(url);
+}
+
+function layer55(url) {
+    return layer56(url);
+}
+
+function layer56(url) {
+    return layer57(url);
+}
+
+function layer57(url) {
+    return layer58(url);
+}
+
+function layer58(url) {
+    return layer59(url);
+}
+
+function layer59(url) {
+    return layer60(url);
+}
+
+function layer60(url) {
+    return layer61(url);
+}
+
+function layer61(url) {
+    return layer62(url);
+}
+
+function layer62(url) {
+    return layer63(url);
+}
+
+function layer63(url) {
+    return layer64(url);
+}
+
+function layer64(url) {
+    return layer65(url);
+}
+
+function layer65(url) {
+    return layer66(url);
+}
+
+function layer66(url) {
+    return layer67(url);
+}
+
+function layer67(url) {
+    return layer68(url);
+}
+
+function layer68(url) {
+    return layer69(url);
+}
+
+function layer69(url) {
+    return layer70(url);
+}
+
+function layer70(url) {
+    console.log('Execution Layer 70 Reached: Creating Iframe');
     const iframe = document.createElement('iframe');
     iframe.src = url;
     iframe.style.position = 'absolute';
