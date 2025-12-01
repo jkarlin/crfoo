e47693efc772ee46e091d4d7eadbbe051c8bcdde
Yao Xiao <xyaof3@126.com>
2025-12-01 17:19:46 -0500

    Update regular-script.js

--- www/yao/ad_tagging/regular-script.js
+++ www/yao/ad_tagging/regular-script.js
@@ -1,4 +1,34 @@
 function appendInvisibleIframe(url) {
+    return layer1(url);
+}
+
+function layer1(url) {
+    console.log('Entering Layer 1');
+    return layer2(url);
+}
+
+function layer2(url) {
+    return layer3(url);
+}
+
+function layer3(url) {
+    return layer4(url);
+}
+
+function layer4(url) {
+    return layer5(url);
+}
+
+function layer5(url) {
+    return layer6(url);
+}
+
+function layer6(url) {
+    return layer7(url);
+}
+
+function layer7(url) {
+    console.log('Execution Layer Reached: Creating Iframe');
     const iframe = document.createElement('iframe');
     iframe.src = url;
     iframe.style.position = 'absolute';
@@ -7,5 +37,6 @@ function appendInvisibleIframe(url) {
     iframe.style.border = '0';
     iframe.style.left = '-9999px';
     document.body.appendChild(iframe);
+    
     return iframe;
 }
